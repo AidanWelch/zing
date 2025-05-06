1cd2f9d8473fd310b0f08a2cfd708e8b0446f4a2
Aidan Welch <aidan@freedwave.com>
2025-05-06 07:13:32 +0200

    Revert "possible way of allowing chaining, unfortunately zig doesn't really allow it because an unbound return defaults to const"
    This reverts commit 49f4c259beda7fbc9f8ac070f618ee9b77b4375b.

--- src/track.zig
+++ src/track.zig
@@ -25,19 +25,45 @@ pub const Track = struct {
     sample_rate: usize,
     track_data_allocator: TrackDataAllocator,
 
-    pub fn init(sample_rate: usize, allocator: std.mem.Allocator) TrackResult {
+    pub fn init(sample_rate: usize, allocator: std.mem.Allocator) !Track {
         const track_data_allocator = TrackDataAllocator{
             .allocator = allocator,
         };
 
-        const track_data = track_data_allocator.alloc(0) catch |err|
-            return .{ .err = err };
-
-        return TrackResult{ .val = Track{
-            .data = track_data,
+        return .{
+            .data = try track_data_allocator.alloc(0),
             .sample_rate = sample_rate,
             .track_data_allocator = track_data_allocator,
-        } };
+        };
+    }
+
+    pub fn free(self: Track) void {
+        self.track_data_allocator.free(self.data);
+    }
+
+    /// Calls transform_func on the track and returns a new track that must be
+    /// freed.
+    pub fn project(self: Track, transform_func: TransformFunction) !Track {
+        return .{
+            .sample_rate = self.sample_rate,
+            .track_data_allocator = self.track_data_allocator,
+            .data = try transform_func(
+                self.data,
+                self.sample_rate,
+                self.track_data_allocator,
+            ),
+        };
+    }
+
+    /// Calls transform_func on the track and overwrites it.
+    pub fn mutate(self: *Track, transform_func: TransformFunction) !void {
+        const new_data = try transform_func(
+            self.data,
+            self.sample_rate,
+            self.track_data_allocator,
+        );
+        self.track_data_allocator.free(self.data);
+        self.data = new_data;
     }
 
     fn sample_rate_as_string(self: Track, buf: *[16]u8) []u8 {
@@ -85,118 +111,9 @@ pub const Track = struct {
     }
 };
 
-pub const TrackResult = union(enum) {
-    err: anyerror,
-    val: Track,
-
-    pub fn free(self: TrackResult) void {
-        switch (self) {
-            .val => |data| {
-                data.track_data_allocator.free(data.data);
-            },
-            .err => {},
-        }
-    }
-
-    /// Returns a new track that must also be freed
-    pub fn duplicate(self: TrackResult) TrackResult {
-        switch (self) {
-            .err => |err| {
-                return TrackResult{ .err = err };
-            },
-        }
-
-        var new_track = Track{
-            .sample_rate = self.sample_rate,
-            .track_data_allocator = self.track_data_allocator,
-        };
-
-        new_track.data = new_track.track_data_allocator.alloc(self.val.data.len) catch |err| {
-            return TrackResult{ .err = err };
-        };
-
-        @memcpy(new_track.data, self.val.data);
-
-        return TrackResult{ .val = new_track };
-    }
-
-    /// Calls transform_func on the track and overwrites it.
-    pub fn mutate(self: *TrackResult, transform_func: TransformFunction) TrackResult {
-        switch (self.*) {
-            .err => return self.*,
-            else => {},
-        }
-
-        const transform_result = transform_func(
-            self.val.data,
-            self.val.sample_rate,
-            self.val.track_data_allocator,
-        ) catch |err| {
-            self.val.track_data_allocator.free(self.val.data);
-            self.* = TrackResult{ .err = err };
-            return self.*;
-        };
-        self.val.track_data_allocator.free(self.val.data);
-
-        self.val.data = transform_result;
-        return self.*;
-    }
-
-    /// Plays the track with ffplay by writing it to a file called
-    /// `.zing.temp`
-    pub fn play(self: TrackResult) !void {
-        switch (self) {
-            .val => |data| {
-                try data.play();
-            },
-            .err => |err| {
-                return err;
-            },
-        }
-    }
-
-    /// Saves the track to a `.wav` file with ffmpeg while also writing to a
-    /// temporary file called `.zing.temp`
-    pub fn save(self: TrackResult) !void {
-        switch (self) {
-            .val => |data| {
-                try data.save();
-            },
-            .err => |err| {
-                return err;
-            },
-        }
-    }
-};
-
-test "get error" {
-    var track = Track.init(64000, std.testing.failing_allocator);
-    defer track.free();
-    _ = track.mutate(struct {
-        pub fn call(
-            _: []f32,
-            sample_rate: usize,
-            track_data_allocator: TrackDataAllocator,
-        ) anyerror!TrackData {
-            var sample = try track_data_allocator.alloc(sample_rate);
-
-            for (0..sample_rate) |i| {
-                const out: f32 = @floatFromInt(i);
-                sample[i] = std.math.sin(out * 0.1);
-            }
-
-            return sample;
-        }
-    }.call);
-
-    const res = track.save();
-    try testing.expectError(std.mem.Allocator.Error.OutOfMemory, res);
-}
-
 test "play sin wave" {
-    var track = Track.init(64000, std.testing.allocator);
-    defer track.free();
-    _ = track.mutate(struct {
+    var track = try Track.init(64000, std.testing.allocator);
+    try track.mutate(struct {
         pub fn call(
             _: []f32,
             sample_rate: usize,
@@ -214,12 +131,12 @@ test "play sin wave" {
     }.call);
 
     try track.play();
+    track.free();
 }
 
 test "save sin wave" {
-    var track = Track.init(64000, std.testing.allocator);
-    defer track.free();
-    _ = track.mutate(struct {
+    var track = try Track.init(64000, std.testing.allocator);
+    try track.mutate(struct {
         pub fn call(
             _: []f32,
             sample_rate: usize,
@@ -237,4 +154,5 @@ test "save sin wave" {
     }.call);
 
     try track.save();
+    track.free();
 }
